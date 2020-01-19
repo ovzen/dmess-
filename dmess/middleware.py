65ffabd8819a8933d67660bb02523f5a49683236
петр козырев <petr.petr3336@gmail.com>
2020-01-19 19:30:23 +0300

    Add custom middleware for automatic admin-panel authenticate from JWT

--- dmess/middleware.py
+++ dmess/middleware.py
@@ -8,13 +8,15 @@ class SimpleMiddleware:
         # One-time configuration and initialization.
 
     def __call__(self, request):
-        token = request.META.get('HTTP_COOKIE').split(' Authentication=')[1].split(';')[0]
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-        val_token=JWTTokenUserAuthentication().get_validated_token(token)
-        authenticated = JWTTokenUserAuthentication().get_user(val_token).id
-        print(User.objects.get(id=authenticated))
-        request.user = User.objects.get(id=authenticated)
+        cookie=request.META.get('HTTP_COOKIE').split(' Authentication=')
+        if len(cookie) > 1:
+            token = cookie[1].split(';')[0]
+            # Code to be executed for each request before
+            # the view (and later middleware) are called.
+            val_token=JWTTokenUserAuthentication().get_validated_token(token)
+            authenticated = JWTTokenUserAuthentication().get_user(val_token).id
+            print(User.objects.get(id=authenticated))
+            request.user = User.objects.get(id=authenticated)
         response = self.get_response(request)
 
         # Code to be executed for each request/response after
