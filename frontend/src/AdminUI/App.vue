748a3f1b173b3156467324385d91448c39ee9074
петр козырев <petr.petr3336@gmail.com>
2020-03-11 17:25:47 +0300

    some visual changes

--- frontend/src/AdminUI/App.vue
+++ frontend/src/AdminUI/App.vue
@@ -6,11 +6,19 @@
       scroll-target="#scrolling-techniques-7"
       elevate-on-scroll
     >
-      <v-toolbar-title style="min-width:100px">AdminUI</v-toolbar-title>
+      <v-toolbar-title style="min-width:100px">
+        AdminUI
+      </v-toolbar-title>
       <v-tabs>
-        <v-tab to="/UserActivity/">Активность пользователей</v-tab>
-        <v-tab to="/DialogActivity/">Активность в чатах</v-tab>
+        <v-tab to="/UserActivity/">
+          Активность пользователей
+        </v-tab>
+        <v-tab to="/DialogActivity/">
+          Активность в чатах
+        </v-tab>
       </v-tabs>
+      <v-spacer />
+      <v-btn href="/" replace>UserUI</v-btn>
     </v-app-bar>
     <v-sheet
       id="scrolling-techniques-7"
