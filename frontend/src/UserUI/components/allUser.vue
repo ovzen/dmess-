435e9f02ddd2927c4cf6842c1afe2556423464de
Nikita Tkachenko <tka4nik@gmail.com>
2020-03-15 20:46:08 +0300

    Fixed spacing

--- frontend/src/UserUI/components/allUser.vue
+++ frontend/src/UserUI/components/allUser.vue
@@ -9,32 +9,35 @@
         <h1 class="text--secondary mb-3">
           Список пользователей
         </h1>
-          <v-card
+        <v-card
           v-for="user in Users"
           :key="user"
-          class="elevation-10 mb-5">
-              <v-layout row>
-                <v-flex
-                  xs4
-                >
-                 <v-card-media
-                                #avatar
-                  height="175px"
-                 ></v-card-media>
-                </v-flex>
-                <v-flex xs8>
-                  <v-card-text>
-                     <h2 class="text--primary">Пользователь: {{ user.username }}</h2>
-                  </v-card-text>
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                    <v-btn class="info">
-                      Открыть
-                    </v-btn>
-                  </v-card-actions>
-                </v-flex>
-              </v-layout>
-          </v-card>
+          class="elevation-10 mb-5"
+        >
+          <v-layout row>
+            <v-flex
+              xs4
+            >
+              <v-card-media
+                #avatar
+                height="175px"
+              ></v-card-media>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">
+                  Пользователь: {{ user.username }}
+                </h2>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="info">
+                  Профиль
+                </v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-app>
