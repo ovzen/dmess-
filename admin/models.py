d118d5ead936311090d794bc277a767aebcce31e
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2020-03-22 16:45:55 +0300

    add created_at, used_at, for_user fields into Invite model

--- admin/models.py
+++ admin/models.py
@@ -1,8 +1,12 @@
 import uuid
 
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Invite(models.Model):
     code = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=True)
+    used_at = models.DateTimeField(default=None, blank=True, null=True)
+    for_user = models.ForeignKey(to=get_user_model(), on_delete=models.SET_NULL, blank=True, null=True)
