1f274a7ba5040dd2601b3f970266d2626fac3f30
Anton Vinogradov <nello1989@mail.ru>
2020-04-06 00:36:37 +0300

    New Sidebar v1

--- frontend/src/UserUI/OldAppvue.vue
+++ frontend/src/UserUI/OldAppvue.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar
       app
-      :value="!(alwaysOnDisplay || expandOnHover)"
+      :value="!(AlwaysOnDisplay || ExpandOnHover)"
       collapse-on-scroll
       dark
       scroll-target="#scrolling-techniques-6"
@@ -22,66 +22,90 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
-
     <v-navigation-drawer
       v-model="drawer"
       absolute
-      app
-      temporary
+      :app="AlwaysOnDisplay"
+      :expand-on-hover="ExpandOnHover"
+      :temporary="!(AlwaysOnDisplay || ExpandOnHover)"
     >
-      <v-list
-        two-line
-        color="#6202EE"
-        dark
-      >
-        <v-list-item>
-
-          <v-list-item-content>
-            <v-list-item-title class="title">{{ username }}</v-list-item-title>
-            <v-list-item-subtitle v-if="isOnline">online</v-list-item-subtitle>
-            <v-list-item-subtitle v-else>offline</v-list-item-subtitle>
-          </v-list-item-content>
-
-          <v-list-item-avatar>
-            <v-img :src="avatar"></v-img>
-          </v-list-item-avatar>
-        </v-list-item>
-      </v-list>
-
-      <v-divider></v-divider>
-      <v-list>
-        Тут будет наполнение сайдбара
-      </v-list>
-
-      <v-divider></v-divider>
-      <v-footer
-        absolute
-        padless
-      >
-        <v-btn
-          fab
-          color="#6202EE"
+      <v-row no-gutters>
+        <v-navigation-drawer
+          v-model="drawer"
           dark
-          top
-          right
+          :app="ExpandOnHover"
+          mini-variant
+          mini-variant-width="56px"
           absolute
         >
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-        <v-card-actions>
-          <v-btn
-            v-for="icon in iconsFooter"
-            :key="icon"
-            icon
+          <v-list>
+            <v-list-item>
+              <v-list-item-action>
+                <v-btn
+                  icon
+                  small
+                  @click="$router.go(-1)"
+                >
+                  <v-icon>mdi-arrow-left-circle</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-action>
+                <v-icon>mdi-settings</v-icon>
+              </v-list-item-action>
+            </v-list-item>
+            <v-divider />
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-img src="https://www.technistone.com/color-range/image-slab/deska_gobi_black_p.jpg" />
+              </v-list-item-avatar>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-img src="https://www.technistone.com/color-range/image-slab/deska_gobi_black_p.jpg" />
+              </v-list-item-avatar>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+        <v-list
+          class="pa-9"
+          nav
+          dense
+          style="padding-right: 0px !important;padding-left: 58px !important;"
+        >
+          <v-list-item-group
+            v-model="group"
           >
-            <v-icon size="24px">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-actions>
-      </v-footer>
+            <v-list-item
+              v-for="dialog in dialogs_for_user"
+              :key="dialog.id"
+              class="grow"
+              @click="openDialog(dialog.id)"
+            >
+              <v-list-item-icon>
+                <v-icon>mdi-chat</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>{{ dialog.name }}</v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+        <div style="position: absolute; top: 80%;">
+          <v-switch
+            v-model="AlwaysOnDisplay"
+            label="Постоянное отображение меню"
+            style="padding-right: 0px !important;padding-left: 58px !important;"
+            :disabled="ExpandOnHover"
+          />
+          <v-switch
+            v-model="ExpandOnHover"
+            label="Мини версия меню"
+            style="padding-right: 0px !important;padding-left: 58px !important;"
+            :disabled="AlwaysOnDisplay"
+          />
+        </div>
+      </v-row>
     </v-navigation-drawer>
-
     <v-sheet
       id="scrolling-techniques-6"
       class="overflow-y-auto"
@@ -115,21 +139,13 @@ export default {
     dialog: false,
     id: 0,
     drawer: false,
-    alwaysOnDisplay: false,
-    expandOnHover: false,
+    AlwaysOnDisplay: false,
+    ExpandOnHover: false,
     group: [],
     for_user: true,
     dialogs_for_user: [],
     username: 'Test',
-    user_id: undefined,
-    avatar: '',
-    isOnline: false,
-    iconsFooter: [
-      'mdi-account-circle',
-      'mdi-message-text',
-      'mdi-room-service',
-      'mdi-settings'
-    ]
+    user_id: undefined
   }),
   watch: {
     // при изменениях маршрута запрашиваем данные снова
@@ -145,7 +161,6 @@ export default {
   mounted () {
     this.getDialogs()
     this.username = jwt.decode(this.$cookie.get('Authentication')).name
-    this.getUserData()
   },
   methods: {
     disconnect () {
@@ -179,15 +194,7 @@ export default {
       this.$router.push({ name: 'allUser' })
     },
     goProfilePage () {
-      this.$router.push({ name: 'Profile', params: { id: this.user_id } })
-    },
-    getUserData () {
-      api.axios
-        .get('/api/users/' + this.user_id)
-        .then(res => {
-          this.avatar = res.data['avatar']
-          this.isOnline = res.data.is_online
-        })
+      this.$router.push({ name: 'Profile', params: { id: this.user_id } });
     }
   }
 }
