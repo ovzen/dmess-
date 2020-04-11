ddbbe3ba4a0168e6a6376bee55ba2130cef3f60d
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2020-04-11 15:37:01 +0300

    add api for statistics

--- admin/urls.py
+++ admin/urls.py
@@ -1,7 +1,9 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import InviteViewSet
+from .views import InviteViewSet, StatisticsView
 
 router = SimpleRouter()
 router.register(r'invites', InviteViewSet)
 urlpatterns = router.urls
+urlpatterns.append(path('statistics/', StatisticsView.as_view()))
--- admin/views.py
+++ admin/views.py
@@ -1,11 +1,27 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
 
 from admin.models import Invite
 from admin.serializers import InviteSerializer
+from datetime import datetime
+
+from main.models import Message, UserProfile
+
+User = get_user_model()
 
 
 class InviteViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminUser]
     serializer_class = InviteSerializer
     queryset = Invite.objects.all()
+
+
+class StatisticsView(APIView):
+    def get(self, request):
+        sign_ups_count = User.objects.filter(date_joined__startswith=datetime.now().date()).count()
+        messages_count = Message.objects.filter(create_date__startswith=datetime.now().date()).count()
+        online_count = UserProfile.objects.filter(is_online=True).count()
+        return Response({"sign_ups_count": sign_ups_count, "messages_count": messages_count, "online_count": online_count})
