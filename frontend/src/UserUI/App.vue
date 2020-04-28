51f972152fa80a0b34759f060559cff1f54030be
петр козырев <petr.petr3336@gmail.com>
2020-04-28 14:50:43 +0300

    fixed render of UserUI

--- frontend/src/UserUI/App.vue
+++ frontend/src/UserUI/App.vue
@@ -112,7 +112,7 @@
               class="justify-center"
             >
               <span class="indigo--text">
-                {{ firstName[0].toUpperCase() }}{{ lastName[0].toUpperCase() }}
+                {{ firstName[0] }}{{ lastName[0] }}
               </span>
             </v-list-item-avatar>
 
