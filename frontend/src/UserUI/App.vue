b6230c73f27dd4b9562dca08a5b2fe4363430821
Ivanov Fedor <ivnfedor@gmail.com>
2020-04-28 15:19:13 +0300

    add bookmark icon

--- frontend/src/UserUI/App.vue
+++ frontend/src/UserUI/App.vue
@@ -29,7 +29,7 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
       <v-btn icon>
-        <v-icon class="material-icons">collections_bookmark</v-icon>
+        <v-icon class="material-icons">mdi-book-multiple</v-icon>
       </v-btn>
 
       <v-menu
