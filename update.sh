540e334e610f446d89943db0ebf08b070072e52d
Andrew Smirnov <smirnov@informatics.ru>
2020-05-01 21:59:55 +0300

    Update CD script. Auth has been fixed.

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "Receiving updated sources"
+git checkout master
+git stash
+git pull
+git stash pop
+
+echo "Installing dependencies"
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py migrate
+
+echo "Getting updated static files"
+cd ~/dmess/frontend || exit
+npm install
+npm run build
+cd ..
+cp -R frontend/dist/* templates/
+python manage.py collectstatic
+
+echo "Restarting server"
+sudo /bin/systemctl restart dmess_celery
+sudo /bin/systemctl restart dmess_daphne
+sudo /bin/systemctl restart nginx
