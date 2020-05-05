5a273f9db2d9514e1fee639b066137647ee1639b
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2020-05-05 19:55:51 +0300

    Add uuid primary key instead of id in Dialog model

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import uuid
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -33,6 +35,7 @@ class Dialog(models.Model):
     users = models.ManyToManyField(User)
     admin_only = models.BooleanField(default=False)
     name = models.CharField(max_length=200)
+    code = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return self.name
