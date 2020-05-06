927895d383a9ed74723c829b6f38c836e50f24e6
Evgenii <rueugen@mail.ru>
2020-05-06 13:39:07 +0700

    add MyProfile and UserProfile

---
+++ frontend/src/UserUI/components/MyProfile.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fluid>
+    <p>Это пример</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Example',
+  data: () => ({
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+</style>
