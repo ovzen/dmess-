160e0040131567218a0bd2db86230b40faa9fc04
Ivanov Fedor <ivnfedor@gmail.com>
2020-05-06 21:08:01 +0300

    Add user profile creation after registration

--- main/signals.py
+++ main/signals.py
@@ -2,8 +2,9 @@
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from rest_registration.signals import user_registered
 
-from main.models import WikiPage
+from main.models import WikiPage, UserProfile
 from main.tasks import markdown_convert
 
 
@@ -12,3 +13,9 @@ def wiki_post_save_callback(**kwargs):
     """Отсылка markdown-кода на преобразование в html-код в момент сохранения модели"""
     instance = kwargs['instance']
     markdown_convert.delay(id=instance.id, md_text=instance.text_markdown)
+
+
+@receiver(user_registered)
+def user_profile_creation(user, request, **kwargs):
+    profile = UserProfile(user=user)
+    profile.save()
