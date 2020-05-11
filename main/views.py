d4b0851af0ec41c8faa4e107ce451e086c3beb72
Ivanov Fedor <ivnfedor@gmail.com>
2020-05-11 16:58:28 +0300

    Remove unused imports

--- main/views.py
+++ main/views.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
