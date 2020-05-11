8fe10cbbcd18c3ca94d41e983aa7f6d97e0cafe6
Ivanov Fedor <ivnfedor@gmail.com>
2020-05-11 11:54:05 +0300

    Add blank=True for dialog

--- main/models.py
+++ main/models.py
@@ -34,7 +34,7 @@ class Dialog(models.Model):
     create_date = models.DateTimeField(auto_now_add=True)
     users = models.ManyToManyField(User)
     admin_only = models.BooleanField(default=False)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, blank=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def last_message(self):
