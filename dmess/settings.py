03c9d8e4261bd11f8c3b7d97f3f45217f9a6bb7f
Ivanov Fedor <ivnfedor@gmail.com>
2020-05-11 16:57:56 +0300

    Upgrade filter backend settings and add add_contact action

--- dmess/settings.py
+++ dmess/settings.py
@@ -76,7 +76,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter'
+    ]
 }
 WSGIPassAuthorization = 'On'
 WEBPACK_LOADER = {
