e87e26f2f881836e023c3f57e76fcc978ee19e27
Галкин Климентий <komok228klim64@gmail.com>
2020-05-13 12:22:27 +0000

    Add js scripts

---
+++ templates/static/js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    var show = true;
+    var countbox = ".row";
+    $(window).on("scroll load resize", function() {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.count').css('opacity', '1');
+            $('.count').spincrement({
+                thousandSeparator: "",
+                duration: 6000
+            });
+
+            show = false;
+        }
+    });
+
+});
