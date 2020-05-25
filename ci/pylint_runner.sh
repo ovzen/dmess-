78952f7ebaa12359b6479d8ac1b4ebd16e6f852e
Aleksandr Pozdnyakov <aa.pozdnyak@yandex.ru>
2020-05-25 22:32:15 +0300

    testing pylint stage in ci

--- ci/pylint_runner.sh
+++ ci/pylint_runner.sh
@@ -3,5 +3,5 @@
 #cd ~/dmess/ || exit
 
 #pylint admin/ dmess/ main/
-pylint --load-plugins pylint_django -j 0 ./main
+pylint --load-plugins pylint_django -j 0 ./public
 exit 0
---
+++ public/test.py
@@ -0,0 +1,12 @@
+"""Файл создан для тестирования codestyle в рамках ci"""
+
+
+def main():
+    """
+    test main
+    """
+    print('test')
+
+
+if __name__ == '__main__':
+    main()
