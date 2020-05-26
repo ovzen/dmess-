c02e3cb7e8e58555252127228262f3199a0b87ab
Aleksandr Pozdnyakov <aa.pozdnyak@yandex.ru>
2020-05-26 21:50:44 +0300

    ci experiments

--- ci/pylint_runner.sh
+++ ci/pylint_runner.sh
@@ -8,3 +8,6 @@ mkdir public
 score=$(sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' pylint.txt)
 anybadge --value=$score --file=public/pylint.svg pylint
 echo "Pylint score was $score"
+
+rm pylint.txt
+exit 0
