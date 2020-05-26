37b1ea9321622ac8959908513a2a0efa7eded125
Aleksandr Pozdnyakov <aa.pozdnyak@yandex.ru>
2020-05-26 22:34:34 +0300

    ci experiments

--- ci/pylint_runner.sh
+++ ci/pylint_runner.sh
@@ -4,9 +4,9 @@
 pylint --load-plugins pylint_django -j 0 ./main > pylint.txt
 
 # get badge
-mkdir public
+#mkdir public
 score=$(sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' pylint.txt)
-anybadge --value=$score --file=public/pylint.svg pylint
+#anybadge --value=$score --file=public/pylint.svg pylint
 echo "test test test Pylint score was $score"
 
 rm pylint.txt
