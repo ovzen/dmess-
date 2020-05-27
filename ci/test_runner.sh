c853b05986c860c624062355cce929f63c594d32
Andrew Smirnov <smirnov@informatics.ru>
2020-05-27 16:33:02 +0300

    Fix coverage folders

--- ci/test_runner.sh
+++ ci/test_runner.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-coverage run --omit venv manage.py test
-coverage report --omit=manage.py
-coverage html --omit=manage.py
+coverage run --omit='venv/*' manage.py test
+coverage report --omit=manage.py,venv
+coverage html --omit=manage.py,venv
 mkdir public
 mv htmlcov public/coverage
