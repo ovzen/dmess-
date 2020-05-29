8946f1001f0f446bc378cfb300c3925e5e842a5f
петр козырев <petr.petr3336@gmail.com>
2020-05-29 21:50:53 +0300

    fixed dialogs

--- main/consumers.py
+++ main/consumers.py
@@ -46,7 +46,7 @@ class ChatConsumer(WebsocketConsumer):
             {
                 'type': 'chat_message',
                 'message': message,
-                'author': author.username,
+                'author': author.id,
                 'create_date': json.dumps(message_obj.create_date, cls=DjangoJSONEncoder)
             }
         )
