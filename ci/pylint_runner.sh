08775866c974ec549fb7ac15172e6a7c55b13411
Andrew Smirnov <smirnov@informatics.ru>
2020-05-30 12:30:24 +0300

    Fix pylint score

--- ci/pylint_runner.sh
+++ ci/pylint_runner.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # run pylint
-pylint $(ls -d */) --load-plugins pylint_django | tee pylint.txt
+shopt -s extglob
+folders=$(ls -d */ | grep -v -e frontend -e templates -e static -e venv -e ci -e docs -e media -e public)
+pylint $folders | tee pylint.txt
 
 # get badge
 mkdir public
@@ -10,7 +12,7 @@ anybadge --value=$score --file=public/pylint.svg pylint
 echo "Pylint score was $score"
 
 # get html
-pylint --load-plugins=pylint_json2html $(ls -d */) --output-format=jsonextended > pylint.json
+pylint --load-plugins=pylint_json2html $folders --output-format=jsonextended > pylint.json
 pylint-json2html -f jsonextended -o public/pylint.html pylint.json
 
 #cleanup
