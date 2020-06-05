59faacd75e20f0bc3826cc197b0bbfa293fcca99
Поздняков Александр <aa.pozdnyak@yandex.ru>
2020-06-05 21:50:24 +0000

    Update frontend/src/AdminUI/main.js, frontend/src/Example/main.js, frontend/src/Login/main.js, frontend/src/UserUI/main.js files

--- frontend/src/Example/main.js
+++ frontend/src/Example/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import '../plugins/axios'
 import App from './App.vue'
 import vuetify from '../plugins/vuetify'
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+Vue.config.debug = false;
+Vue.config.silent = true;
 
 new Vue({
   vuetify,
--- frontend/src/UserUI/main.js
+++ frontend/src/UserUI/main.js
@@ -4,7 +4,10 @@ import App from './App.vue'
 import vuetify from '../plugins/vuetify'
 import router from './router.js'
 import store from './Vuex'
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+Vue.config.debug = false;
+Vue.config.silent = true;
 
 new Vue({
   vuetify,
