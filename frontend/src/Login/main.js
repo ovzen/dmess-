5fc3232c72c214508ad2dc250260c3f2777a7d6b
Aleksandr Pozdnyakov <aa.pozdnyak@yandex.ru>
2020-06-06 00:55:38 +0300

    disable vue debug

--- frontend/src/Login/main.js
+++ frontend/src/Login/main.js
@@ -3,7 +3,10 @@ import '../plugins/axios'
 import App from './App.vue'
 import vuetify from '../plugins/vuetify'
 import router from './router.js'
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+Vue.config.debug = false;
+Vue.config.silent = true;
 
 new Vue({
   vuetify,
